6b13afadccd6c3abb2c2a5070db6a8726e36d4f6
John Adebayo <johnayomide31@gmail.com>
2024-12-24 18:52:38 +0100

    another commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,21 @@ menuButton.addEventListener('click', () => {
     navMenu.classList.toggle('open');
     menuButton.classList.toggle('open');
 });
+
+
+
+
+const today = new Date();
+const currentYear = document.getElementById("Year");
+
+// Display the current year in the footer section
+currentYear.innerHTML = today.getFullYear();
+
+// Display last modified date
+const lastModifiedDate = document.getElementById("lastModified");
+
+// Display the current year in the footer section
+lastModifiedDate.innerHTML = document.lastModified;
+
+
+
