747e4f0558e6e2558923535c7fe0512311f217f3
John Adebayo <johnayomide31@gmail.com>
2025-02-09 18:03:40 +0100

    Refactor funPlace module and discovery page; implement dynamic display of fun places and add new styles

--- chamber/scripts/discovery.js
+++ chamber/scripts/discovery.js
@@ -1,3 +1,26 @@
 import { funPlaces } from "../scripts/modules/funPlace.mjs";
+const div = document.querySelector('#funPlaces');
 
-console.log(funPlaces);
+// displayFunPlaces(data.places);
+
+
+
+
+funPlaces.then(data => {
+    data.places.forEach(place => {
+
+        // const place = data.places;
+        const section = `
+        <section class="place-cards">
+            <h2 class="place-title">${place.name}</h2>
+            <p class="place-description">${place.description}</p>
+            <div class="place-image">
+                <a href="${place.photo_url}"><img src="${place.photo_url}" alt="${place.name}" width="250" height="250">
+           </a> </div>
+            <p class="place-address">${place.address}</p>
+            <button type="button" id="button" class="btn">Learn More</button>
+        </section>
+`;
+        div.innerHTML += section;
+    });
+});
--- chamber/scripts/modules/funPlace.mjs
+++ chamber/scripts/modules/funPlace.mjs
@@ -1,13 +1,18 @@
-const url = "././chamber/data/funPlaces.json";
+const url = "../chamber/data/funPlaces.json";
 
-async function fetchAPI(url) {
+const fetchAPI = async (url) => {
     const response = await fetch(url);
+    // console.log(response.json());
+
     const data = await response.json();
+    // console.log(data);
+
+    console.table(data.places);
     return data;
 }
+console.log(window.location.pathname);
+const funPlaces = fetchAPI(url);
+// console.log(funPlaces);
+// console.log(funPlaces[0]);
 
-
-export const funPlaces = fetchAPI(url);
-console.log(funPlaces);
-
-// export default { funPlaces };
+export { funPlaces };
