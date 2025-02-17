55333142de07b841948f790ac44374b56dfa46d7
John Adebayo <johnayomide31@gmail.com>
2025-02-17 15:33:55 +0100

    Add Font Awesome integration, enhance contact form functionality, and improve thank-you message styling

--- lightup/scripts/contact.js
+++ lightup/scripts/contact.js
@@ -1,8 +1,20 @@
 const formContainer = document.querySelector("#formContainer");
-const form = document.querySelector("form");
+const form = document.querySelector("#contactForm");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.
-    formContainer.textContent = "You have done so well";
-})
+if (form) {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const userName = document.querySelector("#userName");
+        const email = document.querySelector("#email");
+        const content = document.querySelector("#content");
+
+        alert(`${email.value}, ${userName.value}, ${content.value}`);
+        formContainer.innerHTML = `               <div class="thank-you">
+                    <h1 class="thank-you_title">THANK YOU!</h1>
+                    <i class="fa fa-check thank-you-checkmark" id="checkmark"></i>
+                    <p class="thank-you-body" data-lead-id="thank-you-body">Thanks a bunch for reaching out. Our
+                        representative will reach out to you shortly.</p>
+                </div>`
+    });
+}
